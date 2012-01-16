cb212d2450bc6fd91ed73e5b08a68ec27025566f
nareto bh <rennabh@gmail.com>
2012-01-16 15:56:37 +0100

    changed to ecasound

--- choir_sample.py
+++ choir_sample.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python2
 
-#TODO: ecasound:
-#ecasound -a:1  -i playat,0.5,a.wav -ea:100 -a:2 -i playat,1.5,a.wav -ea:80 -a:all  -f:16,1,44100 -o o.wav
 
-#this script uses sox to try and achieve the effect of many people hitting
+#this script uses ecasound to try and achieve the effect of many people hitting
 #a drum. It takes a sample, and performs the folowing operations multiple times:
 #-adds a randomized quantity of silence at the beginning
 #-makes a randomized gain attenuation
-#finally it mixes all down to the outfile and normalizes to -1db.
+#finally it mixes all down to the outfile and normalizes.
 
 import sys
 import os
@@ -29,29 +27,29 @@ def apply(args):
     print args
     
     #let's get some random values for gains and silence padding
-    gains = ran_vector(n, 6)
-    pads = ran_vector(n, 0.05)
+    gains = ran_vector(n, 8)
+    pads = ran_vector(n, 0.03)
 
-    tmpdir = "tmp"
-    os.system("mkdir {0}".format(tmpdir)) #create working directory
-    files = ""
+    cmd = "ecasound "
+
     for i in range(n):
-        #here we define the actual sox command. For each iteration of the for loop, new copies
-        #of the original file are made with a different gain and some silence at the beginning.
-        cmd = "sox {0} {1}/{2}.wav pad {3} gain {4}".format(args[1],tmpdir,i, pads[i], -gains[i]) 
-        print cmd
-        os.system(cmd)
-        files = files + " {0}/{1}.wav".format(tmpdir, i)
-    
+        cmd += "-a:{0} -i playat,{1},{2} -eadb:{3} ".format(i, abs(pads[i]), args[1] ,- abs(gains[i]))
+
     #now mix the files to obtain a single .wav file
-    cmd = "sox -m {0} {1} norm -1".format(files, args[2])
+#    cmd += "-a:all -f:16,1,44100 -o {0}".format(args[2])  
+    cmd += "-a:all -z:mixmode,avg -f:16,1,44100 -o {0}".format(args[2])  
+    print cmd
     os.system(cmd)
 
-    os.system("rm -rf {0}".format(tmpdir)) #remove working directory
+    #normalize file
+    os.system("ecanormalize {0}".format(args[2]))
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
         usage()
+    elif os.path.exists(sys.argv[2]):
+        print "file {0} exists, exiting".format(sys.argv[2])
     else:
         apply(sys.argv)
 
