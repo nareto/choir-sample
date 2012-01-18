de752027ff03be0968ff7ef428b61e8c5d50bfc4
nareto bh <rennabh@gmail.com>
2012-01-18 17:12:37 +0100

    new syntax, multiple samples

--- choir_sample.py
+++ choir_sample.py
@@ -5,7 +5,7 @@ import os
 from random import gauss
 
 def usage():
-    print "USAGE: choir-sample.py infile.wav outfile.wav number_of_copies"
+    print "USAGE: choir-sample.py number_of_copies outfile infile1 [infile2 [infile3 ...[infileN]...]]"
 
 def ran_vector(length, sigma):
     """Returns list with gaussian distributed values with mean 0 """
@@ -15,37 +15,51 @@ def ran_vector(length, sigma):
         ret.append(gauss(0,sigma))
     return ret
 
-def apply(args):
-    n = int(args[3])
-        
+def stereo(file):
+    """Do mono to stereo conversion if necessary - allways returns stereo file"""
+
+    pass #for the moment we suppose all files are stereo
+
+def apply(n,outfile,infiles):
+    N = len(infiles)
     #let's get some random values for gains and silence padding
-    gains = ran_vector(n, 8)
-    pads = ran_vector(n, 0.03)
+    gains = ran_vector(n*N, 8)
+    pads = ran_vector(n*N, 0.04)
 
     #lets build the ecasound command
     cmd = "ecasound "
 
-    for i in range(n):
-        #we add n chains and on each we shift in time and apply gain reduction
-        cmd += "-a:{0} -i playat,{1},{2} -eadb:{3} ".format(i, abs(pads[i]), args[1] ,- abs(gains[i]))
+    #explanation of the following for line: we need an n*N matrix of indexes and a way
+    #to increasingly number its elements. To convince yourself of what is going on uncomment
+    #the print line and eventually the sys.exit() at the end of the for.
+    for i,j,counter in [(i,j,i*(N+3) + j) for i in range(N) for j in range(n)]: 
+        #for every one of the N files in infile we add n chains;
+        #on each of those chains we shift in time and apply gain reduction
+        cmd += "-a:{0} -f:16,i,44100 -i playat,{1},{2} -eadb:{3} ".format(counter, abs(pads[counter]), infiles[i],- abs(gains[counter]))
+        #print counter, " : ", i,j
+    #sys.exit()
 
     #now mixdown all the chains to the output file
-    cmd += "-a:all -z:mixmode,avg -f:16,1,44100 -o {0}".format(args[2])  
+    cmd += "-a:all -z:mixmode,avg -f:16,1,44100 -o {0}".format(outfile)  
     print "We're running this command:","\n", cmd,"\n"
     os.system(cmd)
 
     #finally normalize the file
     print "\n", "Normalizing file:", "\n"
-    os.system("ecanormalize {0}".format(args[2]))
+    os.system("ecanormalize {0}".format(outfile))
 
     print "\n", "Done"
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
+    if len(sys.argv) < 4:
         usage()
-    elif os.path.exists(sys.argv[2]):
-        print "file {0} exists, exiting".format(sys.argv[2])
+        sys.exit()
+    n, outfile = sys.argv[1:3]
+    infiles = sys.argv[3:]
+    if os.path.exists(outfile):
+        print "file {0} exists, exiting".format(outfile)
+        sys.exit()
     else:
-        apply(sys.argv)
+        apply(int(n),outfile,infiles)
 
 
